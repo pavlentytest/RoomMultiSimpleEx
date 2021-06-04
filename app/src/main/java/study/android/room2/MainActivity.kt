761ac0f09b950d19990e5bbb6807b93052d1d68a
vv73 <oivt@ya.ru>
2021-06-04 12:52:37 +0300

    Добавлены элементы интерфейса

--- app/src/main/java/study/android/room2/MainActivity.kt
+++ app/src/main/java/study/android/room2/MainActivity.kt
@@ -1,20 +1,30 @@
 package study.android.room2
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.coroutineScope
-import study.android.room2.entities.Director
-import study.android.room2.entities.School
-import study.android.room2.entities.Student
-import study.android.room2.entities.Subject
-import study.android.room2.entities.relations.StudentSubjectCrossRef
 import kotlinx.coroutines.launch
 
+
+
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rbStudent: RadioButton
+    private lateinit var rbSubject: RadioButton
+    private lateinit var spinner: Spinner
+    private lateinit var listCaption: TextView
+    private lateinit var recyclerView: RecyclerView
+
+
+
     val db by lazy {
         Room.databaseBuilder(
             this,
@@ -25,6 +35,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rbStudent = findViewById(R.id.rbStudent)
+        rbSubject = findViewById(R.id.rbSubject)
+        spinner = findViewById(R.id.spinner)
+        listCaption = findViewById(R.id.listCaption)
+        recyclerView = findViewById(R.id.recyclerView)
+
+        rbStudent.setOnClickListener{
+            listCaption.text = "Student's subjects"
+            // так же должен меняться выпадающий список
+        }
+
+        rbSubject.setOnClickListener{
+            listCaption.text = "Students study"
+            // также должен меняться выпадающий список
+        }
+
+        val items = arrayOf("Здесь", "должен", "располагаться", "список", "учеников", "или предметов")
+
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item, items
+        )
+
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>, view: View?,
+                position: Int, id: Long
+            ) {
+                Toast.makeText(getApplicationContext(), "При выборе должен меняться список на предметы выбранного ученика или учеников, изучающих выбранный предмет", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // TODO Auto-generated method stub
+            }
+        }
 
         val dao = db.schoolDao;
 
@@ -34,9 +81,7 @@ class MainActivity : AppCompatActivity() {
             DataExample.subjects.forEach { dao.insertSubject(it) }
             DataExample.students.forEach { dao.insertStudent(it) }
             DataExample.studentSubjectRelations.forEach { dao.insertStudentSubjectCrossRef(it) }
-            val schoolWithDirector = dao.getSchoolAndDirectorWithSchoolName("Kotlin School")
-            val schoolWithStudents = dao.getSchoolWithStudents("Kotlin School")
         }
-        }
+    }
 
 }
